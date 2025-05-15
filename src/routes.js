6d3e15b06a54a25025fc3930b9b61cc118202c8e
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2025-05-15 12:20:57 -0300

    feat: implementind nested routes

--- src/routes.js
+++ src/routes.js
@@ -3,17 +3,23 @@ import Start from './pages/Start';
 import AboutMe from './pages/AboutMe';
 import Menu from "./components/Menu";
 import Footer from "components/Footer";
+import DefaultPage from "components/DefaultPage";
 
 
 function AppRoutes() {
   return (
   <BrowserRouter>
     <Menu />
+
     <Routes>
-      <Route path="/" element={<Start />} />
-      <Route path="/aboutme" element={<AboutMe />} />
-      <Route path="*" element={<div>Página não encontrada</div>} />
+      <Route path="/" element={<DefaultPage />}>
+        <Route path="/" element={<Start />} />
+        <Route path="/aboutme" element={<AboutMe />} />
+      </Route>
+
+      <Route path="*" element={<div>Page not found</div>} />
     </Routes>
+
     <Footer />
   </BrowserRouter>
   )
